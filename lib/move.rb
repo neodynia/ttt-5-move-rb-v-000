a32e47023eb74cfe17a030540316a6294ccf29a4
Ryan  Boris <ryan.boris@gmail.com>
2017-11-09 22:19:25 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,14 +8,14 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  "#{user_input}".to_i - 1
+  ("#{user_input}".to_i) - 1
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def update_array_at_with(board, index, char)
-  board["#{user_input}".to_i - 1] = "#{char}"
+  board[index] = char
 end
 
 def move(board, index, char="X")
-  
+  update_array_at_with(board, index, char)
 end
